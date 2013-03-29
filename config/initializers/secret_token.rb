2711af44266c88bd9eff1209c566661d36735a8b
sararob <sararob@brandeis.edu>
2013-03-29 15:55:52 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sfmovies::Application.config.secret_token = 'a264171029ec2553aa250917db69cd493844c24a0735aff0301b17530164f0542cb60c09cb0cdb7f0ce0cf0f90551a834b21730b5b79d309f5a25b074b4af65b'

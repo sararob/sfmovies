7e42e370ac544a3bf40f823182666d97f092adde
sararob <sararob@brandeis.edu>
2013-04-01 09:30:52 -0700

    Add Rotten Tomatoes Link

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,6 +10,11 @@ class PagesController < ApplicationController
     if params[:tag]
       @movieselect = Movie.where("title = ?", params[:tag])
       @info = Movie.where("title = ?", params[:tag]).limit(1)
+      
+      require 'open-uri'
+      @encodedtitle = URI::encode(params[:tag])
+      @query = ActiveSupport::JSON.decode(open("http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=xbrfkykvt2zqeusnbdu55gb7&q=" + @encodedtitle + "&page_limit=1").read)
+      @tomatolink = @query["movies"][0]["links"]["alternate"]
     end
     
     @movielist = []
@@ -22,6 +27,6 @@ class PagesController < ApplicationController
     		  prevtitle = title
   		  end
 			end 
-		end 
-  end
+		end #movies.each do
+  end #movies
 end
